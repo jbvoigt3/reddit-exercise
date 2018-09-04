7928ab41de108f25b02a4eb223b614d4d2e13b59
jbvoigt3 <jbvoigt3@gmail.com>
2018-09-04 15:01:55 -0600

    End of exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root "subs#index"
+  
+  resources :sub, only: [:new, :create, :edit, :update]
+
+  get "/subs", to: "subs#index"
+  get "/subs/:id", to: "subs#show"
+  delete "/subs/:id", to: "subs#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
